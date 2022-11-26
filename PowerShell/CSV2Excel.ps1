1f31d2a36ba4cf4c74f88923626f072fe3a46cec
BoΠeShΔdϴw³ <117080369+B0neShAd0w@users.noreply.github.com>
2022-11-26 14:31:45 +0000

    Update CSV2Excel.ps1

--- PowerShell/CSV2Excel.ps1
+++ PowerShell/CSV2Excel.ps1
@@ -1,1 +1,58 @@
+function CSV2Excel {
+    [CmdLetBinding ()]
+    Param (
+        [Parameter (Mandatory = $true)]
+        [String] $InputDirectory,
+        [Parameter (Mandatory = $true)]
+        [String] $OutputDirectory,
+        [Parameter (Mandatory = $true)]
+        [String] $FileName
+    )
+    
+    $xl = New-Object -ComObject Excel.Application
+    $xl.Visible = $false
+    $wb = $xl.Workbooks.Add()
 
+    Get-ChildItem $InputDirectory\*.csv | % {
+        if (Import-Csv $_.FullName) {
+            $csvBook = $xl.Workbooks.Open($_.FullName)
+            $csvBook.ActiveSheet.Copy($wb.Worksheets($wb.Worksheets.Count))
+            $csvBook.Close()
+            Write-Output "`nImporting $($_.FullName)"
+        }
+    }
+
+    try 
+    {
+        $wb.Sheets.Item('sheet1').Delete()
+        Write-Output "`nDeleting unused Sheet1`n"
+    }
+    catch
+    {
+        #OnError
+    }
+
+    try
+    {
+        $wb.SaveAs("$OutputDirectory\$FileName.xlsx")
+        $wb.Close()
+        $xl.Quit()
+        Write-Output "Saving Excel Workbook: $OutputDirectory\$FileName.xlsx`n"
+        Write-Output "Closing application - please be patient...`n"
+
+    $null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($wb); 
+    $null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($xl); 
+    [System.GC]::Collect(); [System.GC]::WaitForPendingFinalizers()
+    }
+    catch
+    {
+        Write-Warning "File name already exists! - EXITING...`n"
+        $null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($wb); 
+        $null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($xl); 
+        [System.GC]::Collect(); [System.GC]::WaitForPendingFinalizers()
+    }
+
+    
+}
+
+CSV2Excel -InputDirectory "C:\Temp" -OutputDirectory "C:\Temp" -FileName "Test-Excel"
