25a507e68b3e8752567a713be44fbabddd55fedf
BoΠeShΔdϴw³ <117080369+B0neShAd0w@users.noreply.github.com>
2022-12-08 09:50:47 +0000

    Update Generate Random User Account Names.ps1

--- PowerShell/Generate Random User Account Names.ps1
+++ PowerShell/Generate Random User Account Names.ps1
@@ -1,1 +1,23 @@
+# Simple code snippet to generate random user account Ids for lab testing.
+$count = 10
+$min = 111111
+$max = 999999
 
+$arr = @('a','c','m','x')
+
+for ($i =0; $i -lt $count; $i++)
+{
+    (($arr) | Get-Random -Count 1 | % {[char]$_}) + (Get-random -Min $min -Maximum $max)
+}
+
+# Example output
+x669570
+a351037
+c272756
+x984260
+c431694
+a597596
+c663235
+x606942
+m346915
+c608987
