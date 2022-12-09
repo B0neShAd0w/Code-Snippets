17b14d5c4e0e606bde8f0dcba797576c1f082869
BoΠeShΔdϴw³ <117080369+B0neShAd0w@users.noreply.github.com>
2022-12-09 07:01:18 +0000

    Update AddPermissionToACL.ps1

--- AddPermissionToACL.ps1
+++ AddPermissionToACL.ps1
@@ -1,1 +1,13 @@
+# To add the permissions on an object:
 
+$path = "C:DFS\Shares\AllUsers "
+$user = "EVIL\H.Simpson"
+$Permiss = "Read, ReadAndExecute, ListDirectory"
+$InheritSettings = "Containerinherit, ObjectInherit"
+$PropogationSettings = "None"
+$RuleType = "Allow"
+$acl = Get-Acl $path
+$perm = $user, $Permiss, $InheritSettings, $PropogationSettings, $RuleType
+$rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $perm
+$acl.SetAccessRule($rule)
+$acl | Set-Acl -Path $path
