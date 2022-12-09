a03a66f5a5612a8a5a01afd63b3be8a65f8520d4
BoΠeShΔdϴw³ <117080369+B0neShAd0w@users.noreply.github.com>
2022-12-09 07:06:19 +0000

    Update DisableFolderInheritance.ps1

--- PowerShell/DisableFolderInheritance.ps1
+++ PowerShell/DisableFolderInheritance.ps1
@@ -1,1 +1,8 @@
+# To disable folder inheritance:
 
+$path = "C:DFS\Shares\AllUsers "
+$acl = Get-ACL -Path $path
+# First $True shows if the folder is protected
+# Second $True specifies if the current NTFS permissions have to be copied
+$acl.SetAccessRuleProtection($True, $True) 
+Set-Acl -Path $path -AclObject $acl
