dafbf3da4d7c6e8ba0e472037014c1b38aaf1dfb
BoΠeShΔdϴw³ <117080369+B0neShAd0w@users.noreply.github.com>
2022-12-09 07:02:35 +0000

    Update RemovePermissionFromACL.ps1

--- RemovePermissionFromACL.ps1
+++ RemovePermissionFromACL.ps1
@@ -1,1 +1,8 @@
+# To remove NTFS permission to access a folder for a user or a group:
 
+$path = "C:DFS\Shares\AllUsers "
+$acl = Get-Acl $path
+$rules = $acl.Access | where IsInherited -eq $false
+$targetrule = $rules | where IdentityReference -eq "EVIL\H.Simpson"
+$acl.RemoveAccessRule($targetrule)
+$acl | Set-Acl -Path $path
